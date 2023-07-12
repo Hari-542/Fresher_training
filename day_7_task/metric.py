532688be6c8185046ffa713bd7ea97bcc30a896c
hariharan <hariharan05042002@gmail.com>
2023-07-12 18:10:28 +0530

    Day seven

--- day_7_task/metric.py
+++ day_7_task/metric.py
@@ -1,29 +1,32 @@
 from sklearn import metrics
 import numpy as np
-from nltk.translate.bleu_score import sentence_bleu,corpus_bleu
-
-p='pepsi'
-c='cola'
-f='fanta'
-
-x_test=[p,p,p,p,c,c,c,c,f,f,f,f]
-y_pred=[p,p,p,c,c,c,f,c,f,c,f,p]
-
-print(metrics.confusion_matrix(x_test,y_pred))
-print(metrics.classification_report(x_test,y_pred))
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.metrics import edit_distance
 
 y_test=[1,0,0,1,1,0,1,1,1]
 y_pred1=[1,0,1,0,1,0,1,0,1]
 prop=[0.1,0.4,0.9,0.3,0.2,0.3,0.9,0.4,0.2]
-print(f'Precision : {metrics.precision_score(y_test,y_pred1)}')
-print(f'Recall : {metrics.recall_score(y_test,y_pred1)}')
-print(f'F1 score : {metrics.f1_score(y_test,y_pred1)}')
-print(f'Roc_Auc : {metrics.roc_auc_score(y_test,prop)}')
+print("accurary : ",metrics.accuracy_score(y_test,y_pred1))
+print('Precision :',metrics.precision_score(y_test,y_pred1))
+print('Recall : ',metrics.recall_score(y_test,y_pred1))
+print('F1 score : ',metrics.f1_score(y_test,y_pred1))
+print('Roc_Auc : ',metrics.roc_auc_score(y_test,prop))
+print('PRC : ',metrics.average_precision_score(y_test,prop))
 
 
-reference = [['that', 'it', 'task']]
-candidate = ['this', 'is', 'test']
-print('bleu score:', sentence_bleu(reference, candidate))
-# print('Individual 3-gram: %f' % sentence_bleu(reference, candidate, weights=(1, 0, 1)))
+reference = [['that' ,'is', 'test','today','morning'],['thsw','is','fine','today','morning']]
+candidate = ['this', 'is', 'test','today','morning']
+print('bleu score 1 gram :', sentence_bleu(reference, candidate,weights=(1,0,0,0)))
+print('bleu score 2 gram :', sentence_bleu(reference, candidate,weights=(0,1,0,0)))
+print('bleu score 3 gram :', sentence_bleu(reference, candidate,weights=(0,0,1,0)))
+print('bleu score 4 gram :', sentence_bleu(reference, candidate,weights=(0,0,0,1)))
+
+x_char="everything is possible"
+y_char="everything is none"
+print('Character error : ',edit_distance(x_char,y_char)/len(x_char))
+
+x_word=x_char.split()
+y_word=y_char.split()
+print('word error : ',edit_distance(x_word,y_word)/len(x_word))
 
 
