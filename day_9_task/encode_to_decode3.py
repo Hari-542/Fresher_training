6d8a45233c204e6f492e13e33a62e7bbeb672f5c
hariharan <hariharan05042002@gmail.com>
2023-07-13 18:18:32 +0530

    Day nine

--- day_9_task/encode_to_decode3.py
+++ day_9_task/encode_to_decode3.py
@@ -7,7 +7,5 @@ with open(path,'r',encoding='utf-8') as file:
 path1='spanish_input.txt'
 with open(path1,'r',encoding='utf-8') as file:
     k=file.read()
-    print(k)
     with open('spanish_output.txt','w+',encoding='utf-8') as file1:
         file1.write(k)
-        print(file1.read())
